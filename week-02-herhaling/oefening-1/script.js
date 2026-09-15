3c0708fd48ada073c211bc4bb78952332b53f39f
SennaBelder <sentheman03@gmail.com>
2026-09-15 10:39:20 +0200

    Opdracht 1 afgerond

--- week-02-herhaling/oefening-1/script.js
+++ week-02-herhaling/oefening-1/script.js
@@ -6,23 +6,23 @@
 
 // 1. Schrijf een functie die twee getallen optelt en het resultaat teruggeeft
 function optellen(a, b) {
-  // TODO
+  return a + b;
 }
 
 // 2. Schrijf een functie die true teruggeeft als het getal even is, anders false
 function isEven(getal) {
-  // TODO
+  return getal % 2 === 0;
 }
 
 // 3. Schrijf een functie die een begroeting teruggeeft: 'Hallo, <naam>!'
 function groet(naam) {
-  // TODO
+  return 'Hallo, ' + naam + '!';
 }
 
 // 4. Schrijf een functie die celsius omrekent naar fahrenheit
 //    formule: celsius * 9 / 5 + 32
 function celsiusNaarFahrenheit(celsius) {
-  // TODO
+  return (celsius * 9) / 5 + 32;
 }
 
 // Dit hoef je niet aan te passen — dit toont je resultaten in de DOM:
