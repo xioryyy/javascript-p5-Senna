c481047cc1f3ebe7de4f6cdfb4739870f31ecf13
SennaBelder <sentheman03@gmail.com>
2026-09-23 14:07:30 +0200

    Oefening2 afgerond

--- week-03-moderne-syntax/oefening-2/script.js
+++ week-03-moderne-syntax/oefening-2/script.js
@@ -2,3 +2,25 @@
 // Knop 1: voeg tekst toe aan #message
 // Knop 2: voeg een <li> toe aan #list met een tekst
 // Knop 3: wissel de klasse 'active' op #message
+let btn = document.getElementById('btn-1');
+let btn2 = document.getElementById('btn-2');
+let btn3 = document.getElementById('btn-3');
+
+btn.addEventListener('click', () => {
+    message.classList.toggle('active');
+    const p = document.createElement('p');
+    p.textContent = 'blablabla';
+    message.appendChild(p);
+
+});
+
+btn2.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = 'Item toegevoegd';
+    list.appendChild(li);
+});
+btn3.addEventListener('click', () => {
+    message.classList.toggle('active');
+    message.textContent = 'active class toegevoegd';
+    btn3.classList.toggle('active');
+});
