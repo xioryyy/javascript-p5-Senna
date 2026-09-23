23eb519e057e477a4f74350de2d883d72e3c5124
SennaBelder <sentheman03@gmail.com>
2026-09-23 13:59:28 +0200

    Eindopdracht week3

--- week-03-moderne-syntax/eindopdracht/script.js
+++ week-03-moderne-syntax/eindopdracht/script.js
@@ -1,3 +1,28 @@
 // Stap 1: Selecteer het formulier en de profielenlijst
 // Stap 2: Luister naar het submit-event, lees de invoervelden uit met .value en toon een profielkaart met innerHTML +=
 // Stap 3 (bonus): Voeg een verwijderknop toe aan elke kaart
+
+let button = document.getElementById("profile-form")
+let section = document.getElementById("profiles-list")
+let naam = document.getElementById("name")
+let functie = document.getElementById("role")
+let afdeling = document.getElementById("department")
+
+button.addEventListener("submit", (event) => {
+     event.preventDefault();
+
+  section.innerHTML = `
+ <article class="profile">
+   <h3>${naam.value}</h3>
+   <p>Functie: ${functie.value}</p>
+   <p>Afdeling: ${afdeling.value}</p>
+   <button type="button" class="delete-button">Verwijder</button>
+ </article> `;
+
+document.querySelector(".delete-button").addEventListener("click", (event) => {
+ event.target.parentElement.remove();
+});
+
+ button.reset();
+});
+
