bab7a8f30f0e76c82e28d423eaec5cd618cba836
SennaBelder <sentheman03@gmail.com>
2026-09-23 14:07:43 +0200

    Uitleg

--- week-03-moderne-syntax/Uitleg/script.js
+++ week-03-moderne-syntax/Uitleg/script.js
@@ -20,3 +20,20 @@ let fruits = ["appel", "banaan", "perzik"];
 for (let fruit of fruits) {
     getName.innerHTML += fruit + ("<br>");
 }
+
+
+
+let title = document.getElementById('title');
+let button = document.getElementById('btn')
+let section = document.getElementById('section')
+
+button.addEventListener('click', () => {
+    title.textContent = 'Ik heb geklikt!';
+
+    title.classList.toggle('active');
+    
+    const p = document.createElement('p');
+    p.textContent = 'Ik heb een paragraaf aangemaakt!';
+    
+    section.appendChild(p);
+});
